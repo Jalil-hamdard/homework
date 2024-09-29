e771d24158e5e55d462f6f31973de25f8b890856
Jalil-hamdard <111887453+Jalil-hamdard@users.noreply.github.com>
2024-09-29 09:29:08 -0700

    Add files via upload

---
+++ MYinfo.java
@@ -0,0 +1,24 @@
+public class MYinfo {
+    public static void main(String[] args) {
+        // MY information
+        String name = "Abdul Jalil Stanikzai";
+        int age = 19; 
+        String major = "Computer Science";
+        String university = "Kabul University";
+        double kankorScore = 331.4;
+        String languages = "English, Persian, Hindi, Pashto";
+        String goal = "Become an expert in software engineering";
+        String currentLearning = "Networking, Java, Python, HTML, Premiere Pro";
+       
+        // Outputting the information
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Major: " + major);
+        System.out.println("University: " + university);
+        System.out.println("Kankor Exam Score: " + kankorScore);
+        System.out.println("Languages Spoken: " + languages);
+        System.out.println("Current Goals: " + goal);
+        System.out.println("Currently Learning: " + currentLearning);
+        
+    }
+}
